b4b975e67019aed9f7964b07eee623042c134dc4
Artem F <artem42fatkulin@gmail.com>
2024-08-06 23:50:56 +0300

    Create clustalo.py

---
+++ protty/clustalo.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+
+class ClustalOmega:
+    '''A simple wrapper around Clustal Omega (http://www.clustal.org/omega/)'''
+
+    def __init__(self, executable: str = 'clustalo') -> None:
+        if os.path.exists(executable):
+            self.executable = executable
+        else:
+            raise FileNotFoundError(f'failed to find Clustal Omega')
+    
+    def __call__(self, infile: str, outfile: str, threads: int = None) -> None:
+        threads = str(threads if threads else os.cpu_count())
+        subprocess.run((self.executable,
+                        '--infile', infile,
+                        '--outfile', outfile,
+                        '--threads', threads))
