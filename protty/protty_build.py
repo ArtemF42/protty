196b5322d1f8f2dbfac7c1708ac7fd13f0f8421d
Artem F <artem42fatkulin@gmail.com>
2024-09-15 00:19:07 +0300

    Modify parse_args: add docstring & help

--- protty/protty_build.py
+++ protty/protty_build.py
@@ -46,12 +46,26 @@ def build_profile_hmm(family: str) -> None:
 
 
 def parse_args() -> argparse.Namespace:
+    '''Parses command line arguments
+
+    Returns:
+        argparse.Namespace: object containing parsed command line arguments
+    '''
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--min', default=5, type=int)
-    parser.add_argument('--max', default=1000, type=int)
-    parser.add_argument('--clustalo', default='clustalo')
-    parser.add_argument('--threads', '-t', default=os.cpu_count(), type=int)
+    parser.add_argument('--min', default=5, type=int,
+                        help='minimum number of records in FASTA file. ' \
+                             'Files with fewer records will be ignored ' \
+                             'during filtering (default: 5)')
+    parser.add_argument('--max', default=1000, type=int,
+                        help='maximum number of records in FASTA file. ' \
+                             'Files with larger number of records will ' \
+                             'be randomly trimmed (default: 1000)')
+    parser.add_argument('--clustalo', default='clustalo',
+                        help='path to Clustal Omega executable')
+    parser.add_argument('--threads', default=os.cpu_count(), type=int,
+                        help='number of threads to use')
+    # parser.add_argument('--skip')
     parser.add_argument('outdir')
 
     return parser.parse_args()
@@ -63,10 +77,6 @@ def main() -> None:
     global OUTDIR
     OUTDIR = args.outdir
 
-    logging.basicConfig(filename=f'{OUTDIR}/protty_build.log',
-                        filemode='w',
-                        format='%(asctime)s %(levelname)s %(message)s',
-                        level=logging.INFO)
     
     for subdir in ('raw', 'filtered', 'msa', 'profiles'):
         os.makedirs(f'{OUTDIR}/{subdir}')
