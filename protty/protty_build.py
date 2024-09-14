78b6cf64672e0ba70b17544048aed2897bd80d4c
Artem F <artem42fatkulin@gmail.com>
2024-09-15 01:50:37 +0300

    Update protty_build.py: rewrite main, add docstrings & minor changes

--- protty/protty_build.py
+++ protty/protty_build.py
@@ -4,32 +4,51 @@ import os
 import random
 import sys
 
+from rich.progress import track
+
 from Bio import SeqIO
 
 from pyhmmer.easel import Alphabet, MSAFile
 from pyhmmer.plan7 import Background, Builder
 
+from .merops import download
 from .wrappers import ClustalOmega, ProgramNotFoundError
 
+logger = logging.getLogger(__name__)
 
-def filter_fasta(filename: str, min_records: int, max_records: int) -> bool:
-    with open(f'{OUTDIR}/raw/{filename}', errors='ignore') as file:
+
+def filter_fasta(outdir: str, filename: str, min_records: int, max_records: int) -> None:
+    '''Filters FASTA files
+    
+    Args:
+        outdir (str): output directory
+        filename (str): name of the FASTA file
+        min_records (int): minimum number of records in FASTA file
+        max_records (int): maximum number of records in FASTA file
+    '''
+    #TODO apply user-defined filter
+
+    with open(f'{outdir}/raw/{filename}', errors='ignore') as file:
         records = list(SeqIO.parse(file, 'fasta'))
 
     if len(records) < min_records:
-        logging.info(f'File {filename} was ignored (< {min_records} records)')
-        return False
+        logger.info(f'File {filename} was ignored (< {min_records} records)')
     else:
         if len(records) > max_records:
             records = random.sample(records, max_records)
-            logging.info(f'File {filename} was trimmed to {max_records} records')
+            logger.info(f'File {filename} was trimmed to {max_records} records')
         
-        SeqIO.write(records, f'{OUTDIR}/filtered/{filename}', 'fasta')
-        return True
+        SeqIO.write(records, f'{outdir}/filtered/{filename}', 'fasta')
 
 
-def build_profile_hmm(family: str) -> None:
-    with MSAFile(f'{OUTDIR}/msa/{family}.fasta', digital=True) as msafile:
+def build_profile_hmm(outdir: str, family: str) -> None:
+    '''Builds profile HMM for the given family
+
+    Args:
+        outdir (str): output directory
+        family (str): name of the family
+    '''
+    with MSAFile(f'{outdir}/msa/{family}.fasta', digital=True) as msafile:
         msa = msafile.read()
         msa.name = family.encode()
     
@@ -39,10 +58,10 @@ def build_profile_hmm(family: str) -> None:
 
     hmm, *_ = builder.build_msa(msa, background)
 
-    with open(f'{OUTDIR}/profiles/{family}.hmm', 'wb') as hmmfile:
+    with open(f'{outdir}/profiles/{family}.hmm', 'wb') as hmmfile:
         hmm.write(hmmfile)
     
-    logging.info(f'Successfully built profile HMM for {family}')
+    logger.info(f'Successfully built profile HMM for {family}')
 
 
 def parse_args() -> argparse.Namespace:
@@ -65,35 +84,48 @@ def parse_args() -> argparse.Namespace:
                         help='path to Clustal Omega executable')
     parser.add_argument('--threads', default=os.cpu_count(), type=int,
                         help='number of threads to use')
-    # parser.add_argument('--skip')
     parser.add_argument('outdir')
 
     return parser.parse_args()
 
 
 def main() -> None:
+    # Parse command line arguments
     args = parse_args()
 
-    global OUTDIR
-    OUTDIR = args.outdir
+    # Configure logger
+    logging.basicConfig(filename=f'{args.outdir}/protty_build.log',
+                        filemode='w',
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        level=logging.INFO)
 
-    
+    # Create output directories
     for subdir in ('raw', 'filtered', 'msa', 'profiles'):
-        os.makedirs(f'{OUTDIR}/{subdir}')
+        path = f'{args.outdir}/{subdir}'
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    # STEP 1. Download MEROPS data
+    if not download(f'{args.outdir}/raw'):
+        sys.exit('An error occurred while downloading MEROPS data. '
+                 'See log file for details.')
+        
+    # STEP 2. Filter FASTA files
+    for filename in track(os.listdir(f'{args.outdir}/raw'),
+                          description='Filtering...'):
+        filter_fasta(args.outdir, filename, args.min, args.max)
     
+    # STEP 3. Align protein sequences & build profile HMMs
     try:
         clustalo = ClustalOmega(args.clustalo)
     except ProgramNotFoundError as error:
         sys.exit('Could not find Clustal Omega')
-
-    download_merops_data()
-
-    for filename in os.listdir(f'{OUTDIR}/raw/'):
+    
+    for filename in track(os.listdir(f'{args.outdir}/filtered'),
+                          description='Building HMMs...'):
         family = os.path.splitext(filename)[0]
-
-        if filter_fasta(filename, args.min, args.max):
-            clustalo.run(f'{OUTDIR}/filtered/{filename}',
-                         f'{OUTDIR}/msa/{family}.fasta',
-                         threads=args.threads)
-            
-            build_profile_hmm(family)
+        clustalo.run(f'{args.outdir}/filtered/{filename}',
+                     f'{args.outdir}/msa/{family}.fasta',
+                     threads=args.threads)
+        build_profile_hmm(family)
